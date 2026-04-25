01d244fe584d6ab98b699f5d25be96ec287a4888
nortetech-admin <admin@nortetech.com.mx>
2026-04-25 15:41:08 -0600

    feat: add display script for bounding box monitor

---
+++ start_display.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rpicam-hello -t 0s \
+  --post-process-file /usr/share/rpi-camera-assets/imx500_mobilenet_ssd.json \
+  --viewfinder-width 1920 \
+  --viewfinder-height 1080 \
+  --width 1920 \
+  --height 1080 \
+  --framerate 30 \
+  --fullscreen
